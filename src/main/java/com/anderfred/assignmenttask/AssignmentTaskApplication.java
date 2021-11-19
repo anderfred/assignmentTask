f1bfe14e78fdcfa723c32c9a5a3fa55163c103a1
fredx <subgorn@gmail.com>
2021-11-19 20:45:22 +0700

    initial commit

---
+++ src/main/java/com/anderfred/assignmenttask/AssignmentTaskApplication.java
@@ -0,0 +1,13 @@
+package com.anderfred.assignmenttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentTaskApplication.class, args);
+    }
+
+}
